c019e66145265b77940d1339c1a045c9439242fb
hui <2889201356@qq.com>
2022-09-24 23:30:12 +0800

    feat: delete demo

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      // http://v.juhe.cn/calendar/day
+      "api": {
+        target: "http://v.juhe.cn/", // 目标地址
+        changeOrigin: true, // 是否改变源、换源
+        ws: true, // web socket开启
+        secure: false, // https检查
+        pathWrite: { // 重写路径
+          "^api": ""
+        }
+      }
+    }
+    // port: 4003,
+    // host: true,
+    // open: true
+    // overlay: {
+    //   warnings: false,
+    //   errors: false
+    // }
+  },
+  // lintOnSave: false
 })
