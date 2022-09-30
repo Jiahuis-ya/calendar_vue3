9ea4180a608c5b4e87caefcea0a30612ec0094c4
hui <2889201356@qq.com>
2022-09-30 17:05:25 +0800

    feat: api使用nginx代理

--- vue.config.js
+++ vue.config.js
@@ -3,20 +3,23 @@ const path = require('path')
 const resolve = (src) => path.resolve(__dirname, src)
 
 module.exports = defineConfig({
+  publicPath: './', // 打包后的文件通过这个路径去访问dist文件
+  outputDir: path.resolve(process.cwd(), './dist'),
   transpileDependencies: true,
   devServer: {
     host: 'localhost',
     open: true,
     port: 8888,
-    proxy: {
-      // 聚合api 
-      "/calendar": {
-        target: "http://v.juhe.cn", // 目标地址
-        changeOrigin: true, // 是否改变源、换源
-        ws: true, // web socket开启
-        secure: false, // https检查
-      },
-    },
+    // TODO:暂时关闭，使用nginx代理
+    // proxy: {
+    //   // 聚合api 
+    //   "/calendar": {
+    //     target: "http://v.juhe.cn", // 目标地址
+    //     changeOrigin: true, // 是否改变源、换源
+    //     ws: true, // web socket开启
+    //     secure: false, // https检查
+    //   },
+    // },
     // overlay: {
     //   warnings: false,
     //   errors: false
